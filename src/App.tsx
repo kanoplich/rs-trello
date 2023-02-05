c2d33bb027788c2cc112e98931527baaec84d7a6
Chizhov Maxim <chizhov98ip@gmail.com>
2023-02-05 13:57:16 +0300

    fix:routing in project section,change routes structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,22 @@ import "./App.css";
 import { Footer } from "./footer/footer";
 import { Header } from "./header/header";
 import { Main } from "./main/main";
+import { Navigate, Route, Routes } from "react-router-dom";
+import WorkspacePage from "./main/workspace/workspace";
+import Projects from "./main/projects/projects";
+import Project from "./main/project/project";
 
 function App() {
   return (
     <>
       <Header />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route path="workspace" element={<WorkspacePage />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="projects/:name/*" element={<Project />} />
+        </Route>
+      </Routes>
       <Footer />
     </>
   );
--- src/main/project/project.tsx
+++ src/main/project/project.tsx
@@ -2,25 +2,23 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import "./project.css";
 
-export function Project() {
-  const history = History;
-  console.log("history", history);
+export default function Project() {
   return (
     <>
       <aside className="aside">
         <div>ADD PROJECT NAME with img?</div>
+        PLANNING
         <div>
-          PLANNING
-          <Link to="/roadmap">ROADMAP</Link>
+          <Link to="board">BOARD</Link>
         </div>
         <div>
-          <Link to="/boards">BOARD</Link>
+          <Link to="roadmap">ROADMAP</Link>
         </div>
       </aside>
-      <section className="naim-section">
+      <section className="main-section">
         <Routes>
-          <Route path="/boards" element={<div>BOARDS</div>} />
-          <Route path="/roadmap" element={<div>ROADMAP</div>} />
+          <Route path="board" element={<div>BOARDS</div>} />
+          <Route path="roadmap" element={<div>ROADMAP</div>} />
         </Routes>
       </section>
     </>
