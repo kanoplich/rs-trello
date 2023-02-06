09a7160ac0e706b1a381252ec5947c92367e1ffe
Hanna Zalatarevich <a.zoloto@mail.ru>
2023-02-06 16:40:35 +0300

    feat: add accounMemu and registarionForm

--- src/main/workspace/workspace.tsx
+++ src/main/workspace/workspace.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import FixedContainer from "../../components/registrationForm";
+
 
 export default function WorkspacePage() {
-  return <section className="workspace-page">WORKSPACE SECTION</section>;
+  return <>
+  <section className="workspace-page">WORKSPACE SECTION
+  <FixedContainer />
+  </section>
+   </>
 }
