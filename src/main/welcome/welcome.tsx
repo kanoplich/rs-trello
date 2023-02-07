810de7f01a1d196d13e4aa30e70f314faf378659
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2023-02-07 23:30:17 +0300

    fead: add worcspace page

--- src/main/welcome/welcome.tsx
+++ src/main/welcome/welcome.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import './welcome.css';
+// import { useState } from "react";
 import { Container, Box, Button } from "@mui/material/";
+import FixedContainer from "../../components/registrationForm";
+
 
 const image: string = require("../../assets/image/JiraScrum.svg").default;
 
 export default function WelcomePage() {
+
+  const [modalActive, setModalActive] = useState(false);
+
   return (
     <>
       <Container maxWidth="xl">
@@ -22,7 +28,7 @@ export default function WelcomePage() {
             }}>
               <h1 className="welcome__title">Team collaboration and incremental delivery starts on the scrum board</h1>
               <p className="welcome__desc">The scrum framework enables software teams to manage complex projects by creating a culture of collaboration. The Jira scrum board is the tool that unites teams around a single goal and promotes iterative, incremental delivery.</p>
-              <Button variant="contained">Get started</Button>
+              <FixedContainer />
             </Box>
           </Box>
           <Box display={{xs: "none", sm: 'block'}} sx={{
--- src/main/workspace/workspace.tsx
+++ src/main/workspace/workspace.tsx
@@ -1,75 +1,127 @@
 import React, { Component } from "react";
 import "./workspace.css"
-import FixedContainer from "../../components/registrationForm";
 import { Container, Box, Card, CardContent, CardActions, Button } from "@mui/material/";
+import { Link } from 'react-router-dom';
+import { routes } from "../main";
 
 const icon: string = require("../../assets/icon/avatar_5.svg").default;
 
 function CardProject() {
   return (
-    <Card sx={{ 
+    <Card sx={{
       height: 188,
       width: 240,
-      marginTop: "100px",
+      minWidth: 240,
       borderRadius: 2,
       borderLeft: 20,
       borderColor: "#FFBDAD",
     }}>
-    <CardContent>
-      <Box sx={{
-            display: "flex",
-            justifyContent: "flex-start",
-            alignItems: "center",
+      <CardContent>
+        <Box sx={{
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "center",
+        }}
+        >
+          <Box>
+            <img className="workspace__icon" src={icon} alt="Welcome" />
+          </Box>
+          <Box>
+            <p className="workspace__team_title">Team name</p>
+            <p className="workspace__team_desc" >Team-managed software</p>
+          </Box>
+        </Box>
+        <p className="workspace__links">QUICK LINKS</p>
+        <Box>
+          <Link className="workspace__links_link" to="#">
+            <p className="workspace__links_issues">My open issues</p>
+            <div className="workspace__links_count"></div>
+          </Link>
+        </Box>
+        <Box>
+          <Link className="workspace__links_link" to="#">
+            <p className="workspace__links_issues">Done issues</p>
+          </Link>
+        </Box>
+      </CardContent>
+      <div className="workspace__border"></div>
+      <CardActions>
+        <Link to="/projects/:name/board" className="workspace__nav-link">
+          <Button size="small" variant="text" color="inherit" style={{
+            textTransform: "lowercase",
+            paddingTop: 0,
+            paddingBottom: 0,
           }}
-      >
-        <Box>
-          <img className="workspace__icon" src={icon} alt="Welcome" />
-        </Box>
-        <Box>
-          <p className="workspace__team_title">Team name</p>
-          <p className="workspace__team_desc" >Team-managed software</p>
-        </Box>
-      </Box>
-      <p className="workspace__links">QUICK LINKS</p>
-      <Box>
-        <a className="workspace__links_link" href="#">
-          <p className="workspace__links_issues">My open issues</p>
-          <div className="workspace__links_count"></div>
-        </a>
-      </Box>
-      <Box>
-        <a className="workspace__links_link" href="#">
-          <p className="workspace__links_issues">Done issues</p>
-        </a>
-      </Box>
-    </CardContent>
-    <CardActions>
-        <Button size="small" variant="text" color="inherit" style={{textTransform: "lowercase"}}>board</Button>
+          >board</Button>
+        </Link>
       </CardActions>
-  </Card>
+    </Card>
   )
+}
+
+function EmptyCardProject() {
+  return (
+    <Box sx={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      flexDirection: "column",
+    }}>
+      <p className="empty-card__desc">You have no recently viewed projects</p>
+      <Link to={routes[1].path} className="workspace__nav-link_btn">
+        <Button size="medium" variant="contained">
+          <span className="workspace__first-letter">View all projects</span>
+        </Button>
+      </Link>
+    </Box>
+  );
+}
+
+function TitleCardProject() {
+  return (
+    <>
+      <Box sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center"
+      }}>
+        <h4 style={{color: "#172B4D"}}>Recent projects</h4>
+        <Link to={routes[1].path} className="workspace__nav-link_view">View all projects</Link>
+      </Box>
+      <Box sx={{
+        display: "flex",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        gap: 2,
+        position: "relative",
+        paddingBottom: "10px",
+        paddingTop: "2px",
+        overflow: "auto",
+      }}
+      >
+
+        <CardProject />
+        <CardProject />
+        <CardProject />
+        <CardProject />
+        <CardProject />
+
+
+      </Box>
+    </>
+  );
 }
 
 export default function WorkspacePage() {
   return (
     <>
       <section className="workspace-page">
-      <FixedContainer />
+        {/* <FixedContainer /> */}
         <Container maxWidth="xl">
-          <h1>Projects</h1>
-          <Box sx={{
-                  display: "flex",
-                  justifyContent: "flex-start",
-                  alignItems: "center",
-                  gap: 2,
-                  position: "relative",
-              }}
-          >
-            <CardProject />
-            <CardProject />
-            <CardProject />
-            <CardProject />
-          </Box>
+          <h1 className="workspace__title">Workspace</h1>
+
+          {false ? <EmptyCardProject /> : <TitleCardProject />}
+
         </Container>
       </section>
     </>
