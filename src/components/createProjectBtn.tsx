9b8a081ebb69184679fcb921153f95316c3009bf
Chizhov Maxim <chizhov98ip@gmail.com>
2023-02-08 21:57:27 +0300

    feat:redirect to projects when create

--- src/components/createProjectBtn.tsx
+++ src/components/createProjectBtn.tsx
@@ -2,7 +2,10 @@ import React from "react";
 import Button from '@mui/material/Button';
 import { useNavigate } from "react-router-dom";
 
+export let openCreateProject = false;
 export default function CreateBtn({text}:{text:string}){
     const navigate = useNavigate()
-    return  <Button variant="text" sx={{color:"inherit",maxHeight:"2.25rem",margin:"auto 0"}} onClick={() => navigate('/projects')}>{text}</Button>
+    return  <Button variant="text" sx={{color:"inherit",maxHeight:"2.25rem",margin:"auto 0"}} onClick={() => {
+        navigate('/projects')
+    }}>{text}</Button>
 }
