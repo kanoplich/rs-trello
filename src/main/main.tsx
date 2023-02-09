0a2ef1e6901ef060baa38251dd3b83e7da36b9c3
Chizhov Maxim <chizhov98ip@gmail.com>
2023-02-09 10:44:42 +0300

    feat:change create project ntm logic, create modal

--- src/main/main.tsx
+++ src/main/main.tsx
@@ -1,28 +1,29 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import "./main.css";
-import { Header } from "../header/header";
-import { Footer } from "../footer/footer";
-import WorkspacePage from "./workspace/workspace";
-import Projects from "./projects/projects";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import './main.css';
+import { Header } from '../header/header';
+import { Footer } from '../footer/footer';
+import WorkspacePage from './workspace/workspace';
+import Projects from './projects/projects';
+import { createPortal } from 'react-dom';
+import CreateProjectModal from './projects/project-modal';
 export function Main() {
   return (
     <>
       <Header />
-
-      <main className="main">
+      <main className='main'>
         <Outlet />
       </main>
-
+      {createPortal(<CreateProjectModal />, document.body)}
       <Footer />
     </>
   );
 }
 export const routes = [
-  { name: "Workspace", path: "/workspace", element: () => WorkspacePage() },
+  { name: 'Workspace', path: '/workspace', element: () => WorkspacePage() },
   {
-    name: "Projects",
-    path: "/projects",
+    name: 'Projects',
+    path: '/projects',
     element: <Projects />,
   },
 ];
--- src/main/projects/projects.tsx
+++ src/main/projects/projects.tsx
@@ -1,70 +1,27 @@
-import React, { useEffect } from "react";
-import { Link, Outlet } from "react-router-dom";
-import SearchField from "../../components/searchField";
-import "./projects.css";
-import ProjectsTable from "./tableProjects";
-import CreateBtn, { openCreateProject } from "../../components/createProjectBtn";
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import SearchField from '../../components/searchField';
+import './projects.css';
+import ProjectsTable from './tableProjects';
+import CreateBtn from '../../components/createProjectBtn';
 
 export default function Projects() {
   return (
     <>
-      <div className="projects_main">
-        <div className="projects_header">
+      <div className='projects_main'>
+        <div className='projects_header'>
           <h2>Projects</h2>
-          <div className="project_btn">
-            <SearchField/>
-            <CreateBtn text={"Create"} />
+          <div className='project_btn'>
+            <SearchField />
+            <CreateBtn text={'Create'} />
           </div>
-        </div> 
-        <ProjectsTable/>
+        </div>
+        <ProjectsTable />
         <></>
-        <Link to=":name/board">PAGE OF ONE PROJECT WITH DETAILS</Link>
+        <Link to=':name/board'>PAGE OF ONE PROJECT WITH DETAILS</Link>
         <div> CREATE YOUR FIRST PROJECT!</div>
       </div>
-      <CreateProjectModal/>
       <Outlet />
     </>
-  );
-}
-
-
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 320,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
-
-function CreateProjectModal() {
-  const [open, setOpen] = React.useState(openCreateProject);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-  useEffect(() => {},[openCreateProject])
-  return (
-    <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-    </div>
   );
 }
