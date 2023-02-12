3892e3bd6721bd4a742bbd41c6435b0ef3c827ba
Chizhov Maxim <chizhov98ip@gmail.com>
2023-02-12 18:02:59 +0300

    feat:add projects filter favorite ,search on table,scroll table body,add modal inputs usability

--- src/main/projects/tableProjects.tsx
+++ src/main/projects/tableProjects.tsx
@@ -7,48 +7,78 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import StarCheckBox from './starCheckbox';
-import { useSelector } from 'react-redux';
-import { getProjects } from '../../store/selectors';
+import { useDispatch, useSelector } from 'react-redux';
+import { getProjects, getProjectSearchResults } from '../../store/selectors';
 import { Link } from 'react-router-dom';
+import DeleteIcon from '@mui/icons-material/DeleteForever';
+import { IconButton } from '@mui/material';
+import {
+  checkAllProjects,
+  checkProject,
+  deleteProject,
+} from '../../store/actions';
 
 export default function ProjectsTable() {
+  const dispatch = useDispatch();
   const projects = useSelector(getProjects);
+  const checkedProjects = useSelector(getProjectSearchResults);
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} size='small' aria-label='a dense table'>
-        <TableHead>
-          <TableRow>
-            <TableCell align='center' sx={{ width: '1rem' }}>
-              <StarCheckBox />
-            </TableCell>
-            <TableCell align='center'>Name</TableCell>
-            <TableCell align='center'>Key</TableCell>
-            <TableCell align='center'>Type</TableCell>
-            <TableCell align='center'>Lead</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {projects.length > 0 &&
-            projects.map(row => (
-              <TableRow
-                key={row.id}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                className='project_item'
+    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer component={Paper} sx={{ maxHeight: '70vh' }}>
+        <Table
+          stickyHeader
+          sx={{ minWidth: 650 }}
+          size='small'
+          aria-label='a dense table'
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell
+                align='center'
+                sx={{ width: '1rem' }}
+                onClick={() => dispatch(checkAllProjects(true))}
               >
-                <TableCell>
-                  {' '}
-                  <StarCheckBox />
-                </TableCell>
-                <TableCell component='th' scope='row' align='center'>
-                  <Link to={`${row.name}/boards`}>{row.name}</Link>
-                </TableCell>
-                <TableCell align='center'>{row.key}</TableCell>
-                <TableCell align='center'>{row.type}</TableCell>
-                <TableCell align='center'>{row.lead}</TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+                <StarCheckBox check={projects.checkAllProjects} />
+              </TableCell>
+              <TableCell align='center'>Name</TableCell>
+              <TableCell align='center'>Key</TableCell>
+              <TableCell align='center'>Type</TableCell>
+              <TableCell align='center'>Lead</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody sx={{ height: '80%' }}>
+            {projects.projects.length > 0 &&
+              checkedProjects.map(row => (
+                <TableRow
+                  key={row.id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  className='project_item'
+                >
+                  <TableCell onClick={() => dispatch(checkProject(row.id))}>
+                    {' '}
+                    <StarCheckBox check={row.checked} />
+                  </TableCell>
+                  <TableCell component='th' scope='row' align='center'>
+                    <Link to={`${row.name}/boards`}>{row.name}</Link>
+                  </TableCell>
+                  <TableCell align='center'>{row.key}</TableCell>
+                  <TableCell align='center'>{row.type}</TableCell>
+                  <TableCell align='center'>{row.lead}</TableCell>
+                  <TableCell
+                    aria-label='delete'
+                    sx={{ width: '1rem' }}
+                    onClick={() => dispatch(deleteProject(row.id))}
+                  >
+                    <IconButton>
+                      <DeleteIcon />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
   );
 }
--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -12,3 +12,25 @@ export const selectProjectModal: Selector<
 
 export const getProjects: Selector<RootState, RootState['projects']> =
   createSelector([(st: RootState) => st.projects], projects => projects);
+export const getCheckedPojects: Selector<
+  RootState,
+  RootState['projects']['projects']
+> = createSelector([getProjects], projects => {
+  return projects.checkAllProjects === true
+    ? projects.projects.filter(item => item.checked)
+    : projects.projects;
+});
+export const getProjectsSearchField = createSelector(
+  [(state: RootState) => state.projectSearch],
+  search => search
+);
+export const getProjectSearchResults = createSelector(
+  [(state: RootState) => state.projectSearch, getCheckedPojects],
+  (search, state) => {
+    return search.length > 0
+      ? state.filter(
+          item => item.name.includes(search) || item.lead.includes(search)
+        )
+      : state;
+  }
+);
