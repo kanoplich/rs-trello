9bdb696c49665314dca83f5ea3b7779bf37546a1
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2023-02-17 21:49:28 +0300

    feat: add delete cards and tasks function

--- src/main/project/components/projectTodo.tsx
+++ src/main/project/components/projectTodo.tsx
@@ -1,19 +1,66 @@
-import { Card, CardContent, Typography } from '@mui/material';
 import '../project.css';
+import { Card, CardContent, Typography, IconButton } from '@mui/material';
+import ClearIcon from '@mui/icons-material/Clear';
 import { CardsType } from '../../../types';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux/es/hooks/useDispatch';
+import { deleteTaskBoard } from '../../../store/actions';
 
 interface ICardsType {
   data: CardsType
+  idProject: number
+  idCard: number
 }
 
-export function ProjectTodo({ data }: ICardsType) {
+export function ProjectTodo({ data, idProject, idCard }: ICardsType) {
+
+  const [state, setState] = useState(false)
+  const dispatch = useDispatch();
+
+  const deleteTask = () => {
+    dispatch(deleteTaskBoard({
+      idProject: idProject,
+      idCard: idCard,
+      text: '',
+      id: data.id,
+    }))
+  }
+
+  const hoverBtn = () => {
+    return (
+      <IconButton
+        onClick={() => deleteTask()}
+        sx={{
+          borderRadius: "3px",
+          position: 'absolute',
+          right: 0,
+          top: 0,
+        }}>
+        <ClearIcon fontSize="medium" />
+      </IconButton>
+    );
+  }
+
   return (
-    <Card sx={{
-      marginBottom: "7px",
-    }}>
-      <CardContent>
-        <Typography gutterBottom>{data.text}</Typography>
+    <Card
+      onMouseEnter={() => setState(true)}
+      onMouseLeave={() => setState(false)}
+      sx={{
+        marginBottom: "7px",
+        position: 'relative',
+      }}>
+      <CardContent
+        sx={{
+          paddingBottom: '24px',
+        }}
+      >
+        <Typography
+          sx={{
+            width: '200px',
+          }}
+          gutterBottom>{data.text}</Typography>
       </CardContent>
+      {state && hoverBtn()}
     </Card>
   );
 }
